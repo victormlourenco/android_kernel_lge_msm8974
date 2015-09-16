1e4355ebd928866b3d6a977094bac72798ee4723
Victor Lourenço <victormlourenco@gmail.com>
2015-09-15 22:08:48 -0300

    cpufreq: nightmare: adaptations

--- drivers/cpufreq/cpufreq_nightmare.c
+++ drivers/cpufreq/cpufreq_nightmare.c
@@ -86,7 +86,7 @@ static struct nightmare_tuners {
 	.dec_cpu_load = 60,
 #ifdef CONFIG_MACH_LGE
 	.freq_for_responsiveness = 1728000,
-	.freq_for_responsiveness_max = 1958400,
+	.freq_for_responsiveness_max = 2265600,
 #else
 	.freq_for_responsiveness = 1566000,
 	.freq_for_responsiveness_max = 1890000,
@@ -122,42 +122,6 @@ show_one(freq_up_brake, freq_up_brake);
 show_one(freq_step_dec, freq_step_dec);
 show_one(freq_step_dec_at_max_freq, freq_step_dec_at_max_freq);
 show_one(io_is_busy, io_is_busy);
-
-static inline cputime64_t get_cpu_idle_time_jiffy(unsigned int cpu,
-						  cputime64_t *wall)
-{
-	u64 idle_time;
-	u64 cur_wall_time;
-	u64 busy_time;
-
-	cur_wall_time = jiffies64_to_cputime64(get_jiffies_64());
-
-	busy_time  = kcpustat_cpu(cpu).cpustat[CPUTIME_USER];
-	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_SYSTEM];
-	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_IRQ];
-	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_SOFTIRQ];
-	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_STEAL];
-	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_NICE];
-
-	idle_time = cur_wall_time - busy_time;
-	if (wall)
-		*wall = jiffies_to_usecs(cur_wall_time);
-
-	return jiffies_to_usecs(idle_time);
-}
-
-static inline cputime64_t get_cpu_idle_time(unsigned int cpu,
-					    cputime64_t *wall, int io_busy)
-{
-	u64 idle_time = get_cpu_idle_time_us(cpu, io_busy ? wall : NULL);
-
-	if (idle_time == -1ULL)
-		idle_time = get_cpu_idle_time_jiffy(cpu, wall);
-	else if (!io_busy)
-		idle_time += get_cpu_iowait_time_us(cpu, wall);
-
-	return idle_time;
-}
 
 /* sampling_rate */
 static ssize_t store_sampling_rate(struct kobject *a, struct attribute *b,
