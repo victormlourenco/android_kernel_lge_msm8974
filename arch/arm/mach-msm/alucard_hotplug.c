a792e1c3be2a127f07c838944eec449183a2aa0a
Dorimanx <yuri@bynet.co.il>
2015-09-21 01:26:35 +0300

    CPU HOTPLUGS: convert all to use state notifier suspend.
    also converted all to use more simple work queue and merged updates
    to Alucard HotPlug from DEV.
    
    Conflicts:
    	arch/arm/mach-msm/alucard_hotplug.c
    	arch/arm/mach-msm/bricked_hotplug.c
    	arch/arm/mach-msm/intelli_hotplug.c
    	arch/arm/mach-msm/msm_hotplug.c

--- arch/arm/mach-msm/alucard_hotplug.c
+++ arch/arm/mach-msm/alucard_hotplug.c
@@ -23,10 +23,8 @@
 #include <linux/module.h>
 #include <linux/slab.h>
 
-#ifdef CONFIG_POWERSUSPEND
-#include <linux/powersuspend.h>
-#else
-#include <linux/fb.h>
+#ifdef CONFIG_STATE_NOTIFIER
+#include <linux/state_notifier.h>
 #endif
 
 struct hotplug_cpuinfo {
@@ -46,9 +44,7 @@ struct hotplug_cpuinfo {
 
 static DEFINE_PER_CPU(struct hotplug_cpuinfo, od_hotplug_cpuinfo);
 
-#ifndef CONFIG_POWERSUSPEND
 static struct notifier_block notif;
-#endif
 static struct delayed_work alucard_hotplug_work;
 
 static struct hotplug_tuners {
@@ -231,17 +227,14 @@ static void __ref hotplug_work_fn(struct work_struct *work)
 	if (num_online_cpus() > 1) {
 		delay -= jiffies % delay;
 	}
-	
+
 	queue_delayed_work_on(BOOT_CPU, system_wq,
 				&alucard_hotplug_work,
 				delay);
 }
 
-#ifdef CONFIG_POWERSUSPEND
-static void __alucard_hotplug_suspend(struct power_suspend *handler)
-#else
-static void __alucard_hotplug_suspend(void)
-#endif
+#ifdef CONFIG_STATE_NOTIFIER
+static void alucard_hotplug_suspend(void)
 {
 	if (hotplug_tuners_ins.hotplug_enable > 0
 				&& hotplug_tuners_ins.hotplug_suspend == 1 &&
@@ -250,11 +243,7 @@ static void __alucard_hotplug_suspend(void)
 	}
 }
 
-#ifdef CONFIG_POWERSUSPEND
-static void __ref __alucard_hotplug_resume(struct power_suspend *handler)
-#else
-static void __ref __alucard_hotplug_resume(void)
-#endif
+static void alucard_hotplug_resume(void)
 {
 	if (hotplug_tuners_ins.hotplug_enable > 0
 		&& hotplug_tuners_ins.suspended == true) {
@@ -265,38 +254,18 @@ static void __ref __alucard_hotplug_resume(void)
 	}
 }
 
-#ifdef CONFIG_POWERSUSPEND
-static struct power_suspend alucard_hotplug_power_suspend_driver = {
-	.suspend = __alucard_hotplug_suspend,
-	.resume = __alucard_hotplug_resume,
-};
-#else
-static int prev_fb = FB_BLANK_UNBLANK;
-
-static int fb_notifier_callback(struct notifier_block *self,
+static int state_notifier_callback(struct notifier_block *this,
 				unsigned long event, void *data)
 {
-	struct fb_event *evdata = data;
-	int *blank;
-
-	if (evdata && evdata->data && event == FB_EVENT_BLANK) {
-		blank = evdata->data;
-		switch (*blank) {
-			case FB_BLANK_UNBLANK:
-				if (prev_fb == FB_BLANK_POWERDOWN) {
-					/* display on */
-					__alucard_hotplug_resume();
-					prev_fb = FB_BLANK_UNBLANK;
-				}
-				break;
-			case FB_BLANK_POWERDOWN:
-				if (prev_fb == FB_BLANK_UNBLANK) {
-					/* display off */
-					__alucard_hotplug_suspend();
-					prev_fb = FB_BLANK_POWERDOWN;
-				}
-				break;
-		}
+	switch (event) {
+		case STATE_NOTIFIER_ACTIVE:
+			alucard_hotplug_resume();
+			break;
+		case STATE_NOTIFIER_SUSPEND:
+			alucard_hotplug_suspend();
+			break;
+		default:
+			break;
 	}
 
 	return NOTIFY_OK;
@@ -360,26 +329,24 @@ static void hotplug_start(void)
 	if (num_online_cpus() > 1) {
 		delay -= jiffies % delay;
 	}
-	INIT_DELAYED_WORK_DEFERRABLE(&alucard_hotplug_work, hotplug_work_fn);
+
+	INIT_DEFERRABLE_WORK(&alucard_hotplug_work, hotplug_work_fn);
+
 	queue_delayed_work_on(BOOT_CPU, system_wq,
 				&alucard_hotplug_work,
 				delay);
 
-#ifdef CONFIG_POWERSUSPEND
-	register_power_suspend(&alucard_hotplug_power_suspend_driver);
-#else
-	notif.notifier_call = fb_notifier_callback;
-	if (fb_register_client(&notif))
-		pr_err("Failed to register FB notifier callback for Alucard Hotplug\n");
+#ifdef CONFIG_STATE_NOTIFIER
+	notif.notifier_call = state_notifier_callback;
+	if (state_register_client(&notif))
+		pr_err("Failed to register State notifier callback for Alucard Hotplug\n");
 #endif
 }
 
 static void hotplug_stop(void)
 {
-#ifdef CONFIG_POWERSUSPEND
-	unregister_power_suspend(&alucard_hotplug_power_suspend_driver);
-#else
-	fb_unregister_client(&notif);
+#ifdef CONFIG_STATE_NOTIFIER
+	state_unregister_client(&notif);
 	notif.notifier_call = NULL;
 #endif
 	cancel_delayed_work_sync(&alucard_hotplug_work);
