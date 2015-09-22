86265574b30a4159471d7c90b585e410a29d9ceb
Victor Lourenço <victormlourenco@gmail.com>
2015-09-21 21:04:52 -0300

    Add buildscript

---
+++ build.sh
@@ -0,0 +1,19 @@
+if [ $# -gt 0 ]; then
+echo $1 > .version
+fi
+ 
+make ARCH=arm CROSS_COMPILE=/home/victor/UBERTC/scripts/out/arm-linux-androideabi-5.2/bin/arm-linux-androideabi- zImage -j6
+ 
+cp arch/arm/boot/zImage ~/anykernel_d802/
+ 
+cd ~/anykernel_d802/
+ 
+zipfile="RedKernel-rXXX-d802.zip"
+echo "making zip file"
+cp zImage zip/kernel/
+ 
+cd zip/
+rm -f *.zip
+zip -r $zipfile *
+rm -f /kernel/*.zip
+cp *.zip /tmp
